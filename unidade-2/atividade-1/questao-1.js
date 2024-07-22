4029dccab680ef465fc658d3b10981edb26b7578
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-22 18:09:19 -0300

    add question 1 of activity 1 of unit 2

---
+++ unidade-2/atividade-1/questao-1.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = 20;
+
+let z = x + y;
+
+console.log(`O valor de x é: ${x}`);
+console.log(`O valor de y é: ${y}`);
+console.log(`O valor da soma de x e y, armazenado em z, é: ${z}`);
